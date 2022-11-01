3cd7e70fe5906ac9fd8c625d410b0f04ce7ea5f1
AkmalShofy <114986742+AkmalShofy@users.noreply.github.com>
2022-11-01 20:39:19 +0800

    Tugas 5

--- PRAK404-2210817210023-Muhammad Shofy Akmal.py
+++ PRAK404-2210817210023-Muhammad Shofy Akmal.py
@@ -1,15 +1,15 @@
-pil = 1
-while pil == 1 :
+w=1
+while w==1 :
     print("Pilih program\n1. Penjumlahan\n2. Pengurangan\n3. Perkalian\n4. Pembagian\n5. Exit")
     print("Masukkan Pilihan : ", end=''); a = int(input())
-    if a<1 or a>1 : 
+    if a<1 or a>5 : 
         print("Input anda salah, silahkan coba lagi\n")
     elif a==5 : 
-        print("\nTerimakasih, telah menggunakan kalkulator Kholid :)\n"); break
+        print("Terimakasih, telah menggunakan kalkulator Akmal"); break
     else : 
         print("Masukkan Nilai Pertama : ", end=''); b=float(input())
         print("Masukkan Nilai Kedua   : ", end=''); c=float(input())
-        if a == 1 : print("Hasil Penjumlahan antara %.2f dengan %.2f adalah %.2f\n" % (b,c,b+c))
-        elif a == 2 : print("Hasil Pengurangan antara %.2f dengan %.2f adalah %.2f\n" % (b,c,b-c))
-        elif a == 3 : print("Hasil Perkalian antara %.2f dengan %.2f adalah %.2f\n" % (b,c,b*c))
+        if a==1 : print("Hasil Penjumlahan antara %.2f dengan %.2f adalah %.2f\n" % (b,c,b+c))
+        elif a==2 : print("Hasil Pengurangan antara %.2f dengan %.2f adalah %.2f\n" % (b,c,b-c))
+        elif a==3 : print("Hasil Perkalian antara %.2f dengan %.2f adalah %.2f\n" % (b,c,b*c))
         else : print("Hasil Pembagian antara %.2f dengan %.2f adalah %.2f\n" % (b,c,b/c))
