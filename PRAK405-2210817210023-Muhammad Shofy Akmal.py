c668be215b2dcad4ab9f0e65d096dde7c785bf5c
AkmalShofy <114986742+AkmalShofy@users.noreply.github.com>
2022-11-02 00:17:56 +0800

    Add files via upload

--- PRAK405-2210817210023-Muhammad Shofy Akmal.py
+++ PRAK405-2210817210023-Muhammad Shofy Akmal.py
@@ -1,14 +1,14 @@
 a, b = input().split()
 a = int(a) ; b = int(b)
-for i in range(1, a + 1) :
-    j = i 
-    while j > 1 :
-        print("(%d * %d) + "%(j, b), end='') ; j -= 1
-    c = 1 ; hasil = (i * b)
-    while c < i :
-        hasil += (c * b) ; c += 1
-    print("(%d * %d) = %d"%(j, b, hasil))
-d = 1 ; e = 1 ; total = 0
-while d <= a :
-    total += e * b ; e += d + 1 ; d += 1
-print("%d"%(total))
+for i in range(1, a+1) :
+    j=i 
+    while j>1 :
+        print("(%d * %d) + " % (j,b), end='') ; j-=1
+    c=1; h=(i*b)
+    while c<i :
+        h+=(c*b); c+=1
+    print("(%d * %d) = %d" % (j,b,h))
+d=1; e=1; t=0
+while d<=a :
+    t+=e*b; e+=d+1; d+=1
+print("%d"%(t))
